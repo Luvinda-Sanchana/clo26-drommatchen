c0b2c1cacb8d1062e7d62399e2f8af84f826a586
Luvinda-Sanchana <lsweedon@gmail.com>
2026-09-18 12:22:30 +0200

    feat: implementera Match-klassen

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -8,34 +8,55 @@ public class Match
     // Properties — publik get, privat set
     public string Hemmalag
     {
+        get { return _hemmalag; }
+        private set { _hemmalag = value; }
         // get och privat set för Hemmalag 
     }
 
     public string Bortalag
     {
+        get { return _bortalag; }
+        private set { _bortalag = value; }  
         // get och privat set för Bortalag
     }
 
     public string Datum
     {
+        get { return _datum; }
+        private set { _datum = value; } 
         // get och privat set för Datum
     }
 
     // Konstruktor
     public Match(string hemmalag, string bortalag, string datum)
     {
+        _hemmalag = hemmalag;
+        _bortalag = bortalag;
+        _datum = datum;
         // TODO: tilldela de privata fälten
     }
 
     // Metod 1: skriv ut matchens lag och datum
     public void Presentera()
     {
+        Console.WriteLine($"{Hemmalag} vs {Bortalag} - {Datum}");
+        Console.WriteLine();
         // TODO
     }
 
     // Metod 2: skriv ut spelarens namn, nummer och position
     public void AnnounceraMålskytt(Spelare spelare)
     {
+        Console.WriteLine($"MÅÅÅL! #{spelare.Nummer} {spelare.Namn} ({spelare.Position})");
         // TODO
+
     }
+
+        // Kontrollerar om spelaren är matchhjälte
+    public bool ÄrMatchhjälte(Spelare spelare)
+    {
+        return spelare.Mål >= 1;
+    }
+
 }
+
