522fa5d25d47130f68197f0dd7499ba4763d6a8a
Luvinda-Sanchana <lsweedon@gmail.com>
2026-09-18 12:21:15 +0200

    feat: implementera Spelare-klassen

--- Drommatchen/Spelare.cs
+++ Drommatchen/Spelare.cs
@@ -4,26 +4,46 @@ public class Spelare
     private string _namn;
     private int _nummer;
     private string _position;
+    private int _mål;
 
     // Properties — publik get, privat set
     public string Namn
     {
+        get { return _namn; }
+        private set { _namn = value; }
+
         // Skapa get och privat set för Namn
     }
 
     public int Nummer
     {
+        get { return _nummer; }
+        private set { _nummer = value; }
+
         // Skapa get och privat set för Nummer
     }
 
     public string Position
     {
+        get { return _position; }
+        private set { _position = value; }
         // Skapa get och privat set för Position
     }
 
+    public int Mål
+    {
+        get { return _mål; }
+        private set { _mål = value; }
+        // Skapa get och privat set för Mål
+    }
+
     // Konstruktor
-    public Spelare(string namn, int nummer, string position)
+    public Spelare(string namn, int nummer, string position, int mål)
     {
         // TODO: tilldela de privata fälten
+        _namn = namn;
+        _nummer = nummer;
+        _position = position;
+        _mål = mål;
     }
 }
